ac9ead3e8b1733336a94b0d445f7de14bd3e7ef5
JooHyoung Cha <aoikazto@naver.com>
2026-09-08 18:07:15 +0900

    fix: simplify output messages in main function for clarity

--- submission01.py
+++ submission01.py
@@ -15,13 +15,13 @@ def main():
     tensor2 = torch.randn(1024,1024)
     
     torch_result = torch.matmul(tensor1, tensor2)
-    print("torch matmul result:", torch_result)
 
     numpy_matmul_result = numpy_matmul(tensor1.numpy(), tensor2.numpy())
-    print("numpy matmul result:", numpy_matmul_result)
 
     if torch.allclose(torch_result, torch.from_numpy(numpy_matmul_result)):
-        print("The results from PyTorch and NumPy matmul are close.")
+        print("Success")
+    else: 
+        print("Failure")
 
 
 if __name__ == "__main__":
